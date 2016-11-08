87644a92ccc9a7f345d1960eccc78662cf69675e
Adam <adam.dipinto@gmail.com>
2016-11-08 02:52:06 +0000

    Setup request URL to pull Github API info for specified user and repo. Assigned own username and access token in request.

--- download_avatars.js
+++ download_avatars.js
@@ -1,14 +1,22 @@
 var request = require('request');
 
+
 console.log("Welcome to the Github Avatar Downloader!");
+
+var GITHUB_USER = "atom888";
+var GITHUB_TOKEN = "ad3290475c4b987c8b9afbf2c09a6b36b48bd523";
 
 function getRepoContributors(repoOwner, repoName, cb) {
 
+
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+
+  console.log(requestURL);
 };
 
 
 
-getRepoContributors("jquery", "jquery", function(err, result) {
+getRepoContributors("atom888", "github-avatar-downloader", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
 });
